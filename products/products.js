444d2bdd3df589337f5ab7ac992a937c3f7c0b4a
Jena Boehm <boehmjena@gmail.com>
2020-10-05 14:53:59 -0700

    Add object literals

---
+++ products/products.js
@@ -0,0 +1,9 @@
+export const plants = [
+    {
+        name: 'Monstera Deliciosa',
+        image: '../assets/monstera.jpg',
+        price: '$40',
+        size: '12"',
+        isOnSale: true
+    },
+];
